5184a612fe71406be5302171769e4e1109ecb3c8
Jacob cote <jacobcote@Air-de-Jacob.lan>
2024-04-24 16:16:03 -0400

    about me page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,6 +6,15 @@ import Appbar from "$lib/Appbar.svelte";
         <Appbar />
     </div>
     <div class="p-8"></div>
+    <div class="grid grid-cols-3 grid-flow-row-dense ">
+        <img src="../assets/photo.svg" alt="boy" class=" w-96 mx-auto m-0"/>
+        <div class="p-8 col-span-2">
+            <h1 class="text-4xl font-bold text-black">About Me</h1>
+            <p class="text-lg text-zinc-700 py-4 ">I am a software developer with a passion for creating and developing software solutions. I have experience in developing web applications using technologies such as HTML, CSS, JavaScript, and Svelte. I am also proficient in using version control systems such as Git and GitHub. I am currently learning how to use Node.js and Express.js to build server-side applications. I am also learning how to use MongoDB to store and manage data. I am a quick learner and I am always looking for new challenges to improve my skills.</p>
+    </div>
+
+
+    </div>
 
   
 
